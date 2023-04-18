9f617d466c5a29070e78053f33ecfed5bc3c8a95
isidro <ortiz.isidro.159@gmail.com>
2023-04-18 02:24:52 -0300

    none

--- src/components/ProfitCalculator.jsx
+++ src/components/ProfitCalculator.jsx
@@ -1,23 +1,44 @@
 import useData from 'renderer/hooks/useData';
 import Divider from './profit_calculator/Divider';
+// eslint-disable-next-line camelcase
 import { calc_total, getDecimal, _add } from './profit_calculator/helper';
 import Menu from './profit_calculator/Menu';
 import Supplies from './profit_calculator/Supplies';
 
-const ProfitCalculator = () => {
+function getPrices({ ingredientes, venta, supplies }) {
+  const costo = calc_total(ingredientes, supplies);
+  const bruto = venta - costo;
+  const porcentaje = `${getDecimal((bruto * 100) / costo, 4)}%`;
+  return { costo, bruto, porcentaje };
+}
+function updateElement(prevMenu, updatedId, updatedData) {
+  const elementsToUpdate = [];
+  prevMenu.forEach(({ ingredientes, id }) => {
+    ingredientes.forEach(
+      ({ supplieId }) => supplieId === updatedId && elementsToUpdate.push(id)
+    );
+  });
+  const result = prevMenu.map((data) => {
+    const { ingredientes, venta, id } = data;
+    const precios = getPrices({ ingredientes, venta, supplies: updatedData });
+
+    if (elementsToUpdate.includes(id)) return { ...data, precios };
+    return data;
+  });
+
+  // eslint-disable-next-line no-undef
+  electron.handleLists.add('menu', result);
+  return result;
+}
+
+function ProfitCalculator() {
   const [menu, setMenu] = useData('menu');
   const [supplies, setSupplies] = useData('supplies');
-  const getPrices = ({ ingredientes, venta, supplies: asSupplies }) => {
-    const costo = calc_total(ingredientes, asSupplies);
-    const bruto = venta - costo;
-    const porcentaje = getDecimal((bruto * 100) / costo, 4) + '%';
-    return { costo, bruto, porcentaje };
-  };
 
   const handleMenu = (values) => {
     const { nombre, venta, categoria, ingredientes, action, id } = values;
 
-    const precios = getPrices({ ingredientes, venta, supplies: supplies });
+    const precios = getPrices({ ingredientes, venta, supplies });
 
     const obj = {
       nombre,
@@ -31,9 +52,10 @@ const ProfitCalculator = () => {
   };
 
   const handleSupplies = (values) => {
-    console.log(values);
     const { action, cantidad, precio, id, ...others } = values;
+    // eslint-disable-next-line camelcase
     const total_por_gramo = getDecimal(precio / cantidad, 4);
+    // eslint-disable-next-line camelcase
     const obj = { ...others, cantidad, precio, total_por_gramo, id };
     setSupplies((previusValue) => {
       const updatedData = _add(previusValue, obj, action);
@@ -43,25 +65,6 @@ const ProfitCalculator = () => {
 
       return updatedData;
     });
-  };
-
-  const updateElement = (prevMenu, updatedId, updatedData) => {
-    const elementsToUpdate = [];
-    prevMenu.forEach(({ ingredientes, id }) => {
-      ingredientes.forEach(
-        ({ supplieId }) => supplieId === updatedId && elementsToUpdate.push(id)
-      );
-    });
-    const result = prevMenu.map((data) => {
-      const { ingredientes, venta, id } = data;
-      const precios = getPrices({ ingredientes, venta, supplies: updatedData });
-
-      if (elementsToUpdate.includes(id)) return { ...data, precios };
-      return data;
-    });
-
-    electron.handleLists.add('menu', result);
-    return result;
   };
 
   return (
@@ -75,6 +78,6 @@ const ProfitCalculator = () => {
       <Menu menu={menu} supplies={supplies} handle={handleMenu} />
     </div>
   );
-};
+}
 
 export default ProfitCalculator;
